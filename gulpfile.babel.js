d9ab41e83bb8c6d4b140e5f2259a70ae23e76e1b
Jason Shin <visualbbasic@gmail.com>
2017-04-29 17:13:05 +1000

    added lint to gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,7 @@
 import gulp from 'gulp';
 import BrowserSync from 'browser-sync';
 import sass from 'gulp-sass';
+import eslint from 'gulp-eslint';
 
 const browserSync = BrowserSync.create();
 
@@ -10,4 +11,11 @@ gulp.task('babel', () => {
 });
 
 gulp.task('sass', () => {
+});
+
+gulp.task('lint', () => {
+  return gulp.src(['**/*.js'], '!node_modules/**')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
