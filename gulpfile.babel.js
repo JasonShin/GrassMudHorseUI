630671989f1a9c6e9fb3a6c5f9094fb2c78539c1
Jason Shin <visualbbasic@gmail.com>
2017-04-29 16:52:24 +1000

    added eslint skeleton babel

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,13 @@
-/**
- * Created by Shin on 29/04/2017.
- */
+import gulp from 'gulp';
+import BrowserSync from 'browser-sync';
+import sass from 'gulp-sass';
+
+const browserSync = BrowserSync.create();
+
+gulp.task('serve', []);
+
+gulp.task('babel', () => {
+});
+
+gulp.task('sass', () => {
+});
